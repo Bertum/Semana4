35ffd037cb9a6f70d19645652010ef86182e8c67
osanfer1982 <osanfer1982@gmail.com>
2018-12-16 16:24:24 +0100

    solved conflict

--- js/game.js
+++ js/game.js
@@ -21,11 +21,7 @@ function gameLoop() {
 
     //Dibujar hud
     paintHud();
-<<<<<<< HEAD
-
-=======
     character.update();
->>>>>>> 46fa7c9f1dd90f07c5cdf13f438b63ee7e2d9441
     //Dibujar bocadillo de texto
     paintBubble();
 
